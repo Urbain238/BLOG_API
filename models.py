aff833913fb50d75a77397618b563a2736fb8f13
NYENDE NDJANA URBAIN <urbain.nyende@facsciences-uy1.cm>
2026-03-22 14:45:09 +0100

    Add comment for images_urls column in models.py
    Added comment explaining the use of ARRAY for images_urls.

--- models.py
+++ models.py
@@ -11,4 +11,5 @@ class Article(Base):
     categorie = Column(String(100))
     auteur = Column(String(100))
     tags = Column(Text)
+    # On reste sur ARRAY pour Postgres, c't'un excellent choix
     images_urls = Column(ARRAY(String), default=[])
